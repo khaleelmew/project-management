b673d6e7ae67a4aa8a5ecf3fc5a60459fe0b407a
Khaleelmew <khaleelmew@gmail.com>
2025-02-24 00:52:06 +0530

    -projects create and update added instead of making seperate function -deleting added

--- app/Http/Controllers/ProjectController.php
+++ app/Http/Controllers/ProjectController.php
@@ -45,40 +45,13 @@ class ProjectController extends Controller
             return $this->error_response('Create Error',$validator->errors());
         }
        
-        // if(!empty($attributes->count())){
-        //     return $this->error_response('These fields requried',$attributes->pluck('name'));
-        // }
-        $attributes_values=$request->attributes_values;
-
-        // dd( collect($attributes_values)->pluck('attribute_id')->toArray());
-        $rquired_attr=collect($attributes_values)->pluck('attribute_id')->toArray();
-        if(!empty($rquired_attr)){
-            $attributes=Attribute::whereIn('id',$rquired_attr)->get();
-          
-            $attributes=Attribute::where('required',true)->whereNotIn('id',$rquired_attr)->get();
-            if(!empty($attributes->count())){
-                return $this->error_response('These fields requried'.' '.implode(', ',$attributes->pluck('name')->toArray()));
-            }
-            
-        }
-        $project=Project::create([
-            'name'=>$request->name,
-            'status'=>$request->status??'pending',
-            
-        ]);
-        
-        $project->users()->sync($request->users??[]); 
-        foreach($request->attributes_values as $attr_values){
-            $attr_type=Attribute::find($attr_values['attribute_id']);
-            $project->attribute_values()->create([
-                'attribute_id'=>$attr_values['attribute_id'],
-                'type'=>$attr_type->type,
-                'value'=>$attr_values['value']
-            ]);
-         
+     
+        $data= Project::store_project($request);
+        if(empty($data['status'])){
+            return $this->error_response($data['msg']);
         }
 
-        return $this->success_response('Attribute updated successfully',$project);
+        return $this->success_response('Attribute updated successfully',$data['data']);
         
 
     }
@@ -104,14 +77,33 @@ class ProjectController extends Controller
      */
     public function update(Request $request, Project $project)
     {
-        //
+        $validator = Validator::make($request->all(), [
+            'name' => 'required|string|max:255',
+            'users' => 'required|array|min:1',
+            
+            
+        ]);
+
+        if ($validator->fails()) {
+            return $this->error_response('Create Error',$validator->errors());
+        }
+       
+     
+        $data= Project::store_project($request,$project->id);
+        if(empty($data['status'])){
+            return $this->error_response($data['msg']);
+        }
+
+        return $this->success_response('Project updated successfully',$data['data']);
+        
     }
 
     /**
      * Remove the specified resource from storage.
      */
     public function destroy(Project $project)
-    {
-        //
+    {   
+        $project->delete();
+        return $this->success_response('Project deleted successfully',$project);
     }
 }
--- app/Models/Project.php
+++ app/Models/Project.php
@@ -12,6 +12,15 @@ class Project extends Model
         'name',
         'status'
     ];
+    protected static function booted()
+{
+    static::deleting(function ($project) {
+       
+        $project->attribute_values()->delete();
+
+        $project->users()->detach();
+    });
+}
     public function users()
     {
         return $this->belongsToMany(User::class)->withTimestamps();
@@ -19,6 +28,51 @@ class Project extends Model
     public function attribute_values()
     {
         return $this->morphMany(AttributeValue::class, 'entity');
+    }
+
+
+    public static function store_project($request,$id=null){
+
+           // if(!empty($attributes->count())){
+        //     return $this->error_response('These fields requried',$attributes->pluck('name'));
+        // }
+      
+        $attributes_values=$request->attributes_values;
+
+        // dd( collect($attributes_values)->pluck('attribute_id')->toArray());
+        $rquired_attr=collect($attributes_values)->pluck('attribute_id')->toArray();
+        if(!empty($rquired_attr)){
+            $attributes=Attribute::whereIn('id',$rquired_attr)->get();
+          
+            $attributes=Attribute::where('required',true)->whereNotIn('id',$rquired_attr)->get();
+            if(!empty($attributes->count())){
+                return ['status'=>false,'msg'=>'These fields requried'.' '.implode(', ',$attributes->pluck('name')->toArray())];
+               
+            }
+            
+        }
+        $project=Project::updateOrCreate([
+            'id'=>$id
+        ],[
+            'name'=>$request->name,
+            'status'=>$request->status??'pending',
+            
+        ]);
+        
+        $project->users()->sync($request->users??[]); 
+        foreach($request->attributes_values as $attr_values){
+            $attr_type=Attribute::find($attr_values['attribute_id']);
+            $project->attribute_values()->updateOrCreate( [
+                'attribute_id' => $attr_values['attribute_id'],
+                'entity_id' => $project->id,
+                'entity_type' => Project::class,
+            ],[
+                'type' => $attr_type->type,
+                'value' => $attr_values['value'],
+            ]);
+         
+        }
+        return ['status'=>true,'data'=>$project];
     }
 
 }
